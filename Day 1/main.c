df2ed7f7ae88fc7eb9254f5c94be65cf03862fe8
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2025-02-24 21:11:06 +0530

    Refactor main.c to include math.h, add cube calculation, and enhance comments on arithmetic instructions

--- Day 1/main.c
+++ Day 1/main.c
@@ -1,6 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-int main(){
+int main()
+{
 
     /*
     // Write a program to calculate of rectangle.
@@ -12,13 +14,57 @@ int main(){
     scanf("%d",&width);
     int sizeOfRectangle = hight*width;
     printf("Size of Rectangle :%d",sizeOfRectangle);
-    */
+
 
     // Take a number(n) from  user & output its cube(n*n*n)
     int n;
     printf("Please Enter of cude side N:");
     scanf("%d",&n);
     printf("Cube size: %d", n*n*n);
-    
+    */
+
+    // Arithmetic Inctructions
+    /*
+     // pow function use
+     int power = pow(7,2); // pow come for -> #include <math.h>
+     printf("%d\n",power);
+
+     // Moduler operator -> %
+     int module = 11%3;
+     printf("%d", module); // 8/7 remeder 1
+     // modulo operator work in integer(int).
+
+
+    // Type Conversion
+
+    // int op int -> int
+    printf("%d\n", 2 * 3);
+    // int op float -> float
+    printf("%f\n", 2 * 3.13);
+    // float op float -> float
+    printf("%f\n", 2.4 * 3.2);
+
+
+    int a=(int) 1.999999;
+    printf("%d",a);
+
+   int b = 4+9*10; // 94
+   int a = 4*3/6*2; // 4
+   // ((4*3)/6)*2
+   // (12/6)*2
+   // 2*2
+   // 4
+*/
+
+    /*
+   printf("%d\n", 4==4); // 1 -> true
+   printf("%d\n", 4==3); // 0 -> Falsh
+   */
+
+    int x;
+    printf("Enter number:");
+    scanf("%d", &x);
+    printf("%d", x / 2 == 0 );
+
     return 0;
 }
