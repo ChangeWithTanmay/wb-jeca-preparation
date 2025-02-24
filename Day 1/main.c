aa82f376de7620b23adcdb4e769d2b4699e2741e
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2025-02-24 11:52:23 +0530

    Add cube calculation feature and comment out rectangle calculation

--- Day 1/main.c
+++ Day 1/main.c
@@ -2,7 +2,9 @@
 
 int main(){
 
+    /*
     // Write a program to calculate of rectangle.
+
     int hight, width;
     printf("Enter Number of hight:");
     scanf("%d",&hight);
@@ -10,6 +12,13 @@ int main(){
     scanf("%d",&width);
     int sizeOfRectangle = hight*width;
     printf("Size of Rectangle :%d",sizeOfRectangle);
+    */
+
+    // Take a number(n) from  user & output its cube(n*n*n)
+    int n;
+    printf("Please Enter of cude side N:");
+    scanf("%d",&n);
+    printf("Cube size: %d", n*n*n);
     
     return 0;
 }
